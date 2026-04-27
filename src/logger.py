670880c9a6f6258a19441b6bdcb25608ffa8d676
somnexia <georgii.giblov@gmail.com>
2026-04-27 13:15:39 +0300

    btc-price-script/ │ ├── src/ │   ├── main.py │   ├── service.py │   ├── config.py │   ├── logger.py │   ├── scheduler.py │   └── __pycache__/ │ ├── logs/ │   └── btc_log.txt │ ├── requirements.txt ├── .gitignore └── README.md     featch: проверяет наличие папки создаёт её пишет лог

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,10 @@
 
 # “сохранить цену в лог”
 from datetime import datetime
+import os
 
 def save(price):
+    os.makedirs("logs", exist_ok=True)
+
     with open("../logs/btc_log.txt", "a") as f:
         f.write(f"{datetime.now()} BTC: {price} EUR\n")
